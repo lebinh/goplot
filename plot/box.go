9aef86e00152e2faec4e3b27aeed676fb8213c90
Binh Le <lebinh.it@gmail.com>
2017-02-23 00:51:56 -0800

    stats calculation for box plot

--- plot/box.go
+++ plot/box.go
@@ -3,17 +3,46 @@ package plot
 import (
 	"bytes"
 	"fmt"
+	"sort"
+	"math"
 )
 
 type BoxAndWhisker struct {
-	LeftWhisker  int
-	Left         int
-	Mid          int
-	Right        int
-	RightWhisker int
+	LeftWhisker  float64 // smallest x that x >= Q1 - 1.5IQR
+	Left         float64 // == Q1
+	Mid          float64 // == Q2
+	Right        float64 // == Q3
+	RightWhisker float64 // largest x that x <= Q3 + 1.5IQR
 }
 
-func (box BoxAndWhisker) validate() {
+type NormalizedBoxAndWhisker struct {
+	LeftWhisker  int // smallest x that x >= Q1 - 1.5IQR
+	Left         int // == Q1
+	Mid          int // == Q2
+	Right        int // == Q3
+	RightWhisker int // largest x that x <= Q3 + 1.5IQR
+}
+
+type Quartiles struct {
+	First, Second, Third float64
+}
+
+func (box BoxAndWhisker) normalize(maxWidth int) NormalizedBoxAndWhisker {
+	// ಠ_ಠ  this is ridiculous but I don't know a better way to do this max
+	max := math.Max(box.LeftWhisker, math.Max(box.Left, math.Max(box.Mid, math.Max(box.Right, box.RightWhisker))))
+	normalize := func(val float64) int  {
+		return int(val / max * float64(maxWidth))
+	}
+	return NormalizedBoxAndWhisker{
+		normalize(box.LeftWhisker),
+		normalize(box.Left),
+		normalize(box.Mid),
+		normalize(box.Right),
+		normalize(box.RightWhisker),
+	}
+}
+
+func (box NormalizedBoxAndWhisker) validate() {
 	if !(box.LeftWhisker <= box.Left &&
 		box.Left <= box.Mid &&
 		box.Mid <= box.Right &&
@@ -23,16 +52,80 @@ func (box BoxAndWhisker) validate() {
 	}
 }
 
-func drawBox(box BoxAndWhisker) string {
+func calculateBoxAndWhisker(values []float64) BoxAndWhisker {
+	if len(values) == 0 {
+		return BoxAndWhisker{}
+	}
+
+	// sort a copy of input values
+	sorted := make([]float64, len(values))
+	copy(sorted, values)
+	sort.Float64s(sorted)
+
+	quartiles := quartilesOfSorted(sorted)
+	box := BoxAndWhisker{Left: quartiles.First, Mid: quartiles.Second, Right: quartiles.Third}
+
+	oneHalfIQR := (quartiles.Third - quartiles.First) * 1.5
+	for i := 0; i < len(sorted); i++ {
+		if sorted[i] >= quartiles.First-oneHalfIQR {
+			box.LeftWhisker = sorted[i]
+			break
+		}
+	}
+	for i := len(sorted) - 1; i >= 0; i-- {
+		if sorted[i] <= quartiles.Third+oneHalfIQR {
+			box.RightWhisker = sorted[i]
+			break
+		}
+	}
+	return box
+}
+
+func quartilesOfSorted(sortedValues []float64) Quartiles {
+	if len(sortedValues) == 0 {
+		return Quartiles{}
+	}
+
+	// using method 2 of computing quartiles from https://en.wikipedia.org/wiki/Quartile
+	// use the median to divide the ordered data set into two halves.
+	median := medianOfSorted(sortedValues)
+	mid := int(len(sortedValues) / 2)
+	var firstHalf, secondHalf []float64
+	if len(sortedValues)%2 == 0 {
+		firstHalf = sortedValues[:mid]
+	} else {
+		// including the median value in the first half if odd number of values
+		firstHalf = sortedValues[:mid+1]
+	}
+	secondHalf = sortedValues[mid:]
+	q1 := medianOfSorted(firstHalf)
+	q3 := medianOfSorted(secondHalf)
+	return Quartiles{q1, median, q3}
+}
+
+func medianOfSorted(sortedValues []float64) float64 {
+	if len(sortedValues) == 0 {
+		return 0
+	}
+
+	mid := int(len(sortedValues) / 2)
+	if len(sortedValues)%2 == 0 {
+		return (sortedValues[mid-1] + sortedValues[mid]) / 2
+	} else {
+		return sortedValues[mid]
+	}
+}
+
+func drawBox(box NormalizedBoxAndWhisker) string {
 	box.validate()
-	var buf = new(bytes.Buffer)
-	drawBoxTop(buf, box)
-	drawBoxMid(buf, box)
-	drawBoxBottom(buf, box)
+	buf := new(bytes.Buffer)
+	drawBoxTop(box, buf)
+	drawBoxMid(box, buf)
+	drawBoxBottom(box, buf)
 	return buf.String()
 }
 
-func drawBoxTop(buf *bytes.Buffer, box BoxAndWhisker) {
+func drawBoxTop(box NormalizedBoxAndWhisker, buf *bytes.Buffer) {
 	//  ¯\_(ツ)_/¯
 	writeRepeat(buf, " ", box.Left)
 	switch {
@@ -56,7 +149,7 @@ func drawBoxTop(buf *bytes.Buffer, box BoxAndWhisker) {
 	buf.WriteString("\n")
 }
 
-func drawBoxMid(buf *bytes.Buffer, box BoxAndWhisker) {
+func drawBoxMid(box NormalizedBoxAndWhisker, buf *bytes.Buffer) {
 	//  ¯\_(ツ)_/¯
 	writeRepeat(buf, " ", box.LeftWhisker)
 	if box.LeftWhisker < box.Left {
@@ -90,7 +183,7 @@ func drawBoxMid(buf *bytes.Buffer, box BoxAndWhisker) {
 	buf.WriteString("\n")
 }
 
-func drawBoxBottom(buf *bytes.Buffer, box BoxAndWhisker) {
+func drawBoxBottom(box NormalizedBoxAndWhisker, buf *bytes.Buffer) {
 	//  ¯\_(ツ)_/¯
 	writeRepeat(buf, " ", box.Left)
 	switch {
--- plot/box_test.go
+++ plot/box_test.go
@@ -1,8 +1,11 @@
 package plot
 
 import (
+	"math"
 	"testing"
 )
+
+const delta = 1e-6
 
 //  ¯\_(ツ)_/¯
 var box1 = `
@@ -31,17 +34,89 @@ var box4 = `
 
 func TestDrawBox(t *testing.T) {
 	tests := []struct {
-		box  BoxAndWhisker
+		box  NormalizedBoxAndWhisker
 		want string
 	}{
-		{BoxAndWhisker{0, 0, 0, 0, 0}, box1},
-		{BoxAndWhisker{0, 3, 7, 10, 14}, box2},
-		{BoxAndWhisker{2, 4, 4, 8, 10}, box3},
-		{BoxAndWhisker{0, 3, 6, 6, 10}, box4},
+		{NormalizedBoxAndWhisker{0, 0, 0, 0, 0}, box1},
+		{NormalizedBoxAndWhisker{0, 3, 7, 10, 14}, box2},
+		{NormalizedBoxAndWhisker{2, 4, 4, 8, 10}, box3},
+		{NormalizedBoxAndWhisker{0, 3, 6, 6, 10}, box4},
 	}
 	for _, test := range tests {
 		if got := drawBox(test.box); got != test.want {
 			t.Errorf("drawBox(%v):\n%v, expected:\n%v", test.box, got, test.want)
 		}
 	}
+}
+
+func TestCalculateBoxAndWhisker(t *testing.T) {
+	tests := []struct {
+		values []float64
+		want   BoxAndWhisker
+	}{
+		{[]float64{}, BoxAndWhisker{}},
+		{[]float64{1}, BoxAndWhisker{1, 1, 1, 1, 1}},
+		{[]float64{1, 2}, BoxAndWhisker{1, 1, 1.5, 2, 2}},
+		{[]float64{1, 2, 3}, BoxAndWhisker{1, 1.5, 2, 2.5, 3}},
+		{[]float64{1, 2, 3, 4}, BoxAndWhisker{1, 1.5, 2.5, 3.5, 4}},
+		{[]float64{1, 2, 3, 4, 5}, BoxAndWhisker{1, 2, 3, 4, 5}},
+		{[]float64{1, 2, 3, 4, 5}, BoxAndWhisker{1, 2, 3, 4, 5}},
+	}
+	for _, test := range tests {
+		if got := calculateBoxAndWhisker(test.values); !equalBoxAndWhisker(got, test.want) {
+			t.Errorf("calculateBoxAndWhisker(%v) = %v, expected: %v", test.values, got, test.want)
+		}
+	}
+}
+
+func equalBoxAndWhisker(b1, b2 BoxAndWhisker) bool {
+	return closeEnough(b1.LeftWhisker, b2.LeftWhisker) &&
+		closeEnough(b1.Left, b2.Left) &&
+		closeEnough(b1.Mid, b2.Mid) &&
+		closeEnough(b1.Right, b2.Right) &&
+		closeEnough(b1.RightWhisker, b2.RightWhisker)
+}
+
+func closeEnough(f1, f2 float64) bool {
+	return math.Abs(f1 - f2) < delta
+}
+
+func TestCalculateQuartiles(t *testing.T) {
+	tests := []struct {
+		values []float64
+		want   Quartiles
+	}{
+		{[]float64{}, Quartiles{}},
+		{[]float64{1}, Quartiles{1, 1, 1}},
+		{[]float64{1, 2}, Quartiles{1, 1.5, 2}},
+		{[]float64{1, 2, 3}, Quartiles{1.5, 2, 2.5}},
+		{[]float64{1, 2, 3, 4}, Quartiles{1.5, 2.5, 3.5}},
+		{[]float64{1, 2, 3, 4, 5}, Quartiles{2, 3, 4}},
+	}
+	for _, test := range tests {
+		if got := quartilesOfSorted(test.values); !equalQuartiles(got, test.want) {
+			t.Errorf("quartilesOfSorted(%v) = %v, expected: %v", test.values, got, test.want)
+		}
+	}
+}
+
+func TestMedianOfSorted(t *testing.T) {
+	tests := []struct {
+		values []float64
+		want   float64
+	}{
+		{[]float64{}, 0},
+		{[]float64{1}, 1},
+		{[]float64{1, 2}, 1.5},
+		{[]float64{1, 2, 3}, 2},
+	}
+	for _, test := range tests {
+		if got := medianOfSorted(test.values); !closeEnough(got, test.want) {
+			t.Errorf("medianOfSorted(%v) = %v, expected: %v", test.values, got, test.want)
+		}
+	}
+}
+
+func equalQuartiles(q1, q2 Quartiles) bool {
+	return closeEnough(q1.First, q2.First) && closeEnough(q1.Second, q2.Second) && closeEnough(q1.Third, q2.Third)
 }
